f48768d4bded17412010b9cf8f18684baa65c8c1
bambbdd <childu096@gmail.com>
2025-11-05 21:14:02 +0900

    articles get

--- demo/src/main/java/com/bambbdd/DemoApplication.java
+++ demo/src/main/java/com/bambbdd/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.bambbdd.demo;
+package com.bambbdd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ demo/src/main/java/com/bambbdd/global/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.bambbdd.global.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers("/**").permitAll())
+                .csrf((csrf) -> csrf
+                        .ignoringRequestMatchers("/h2-console/**")
+                )
+                .headers((headers) -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                );
+
+        return http.build();
+    }
+}
