0113a202d160769c729e4dc0a0229257edd56b32
bambbdd <childu096@gmail.com>
2025-12-11 12:06:52 +0900

    init Data

--- demo/src/main/java/com/bambbdd/domain/article/service/ArticleService.java
+++ demo/src/main/java/com/bambbdd/domain/article/service/ArticleService.java
@@ -1,4 +1,39 @@
 package com.bambbdd.domain.article.service;
 
+import com.bambbdd.domain.article.entity.Article;
+import com.bambbdd.domain.article.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    public List<Article> getList() { // 다건
+        return articleRepository.findAll();
+    }
+
+    public Article getArticle(Long id) { // 단건
+        Optional<Article> oq = articleRepository.findById(id);
+
+        if ( oq.isEmpty() )  {
+            return null;
+        }
+
+        return oq.get();
+    }
+
+    public void create(String subject, String content) {
+        Article article = Article.builder()
+                .subject(subject)
+                .content(content)
+                .build();
+
+        articleRepository.save(article);
+    }
+
 }
