ad1d94ecb834f1c1b1fd9339466826598f076930
bambbdd <childu096@gmail.com>
2026-01-24 15:33:07 +0900

    Member MVC Test Setting

---
+++ demo/src/main/java/com/bambbdd/domain/member/entity/Member.java
@@ -0,0 +1,18 @@
+package com.bambbdd.domain.member.entity;
+
+import com.bambbdd.global.jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class Member extends BaseEntity {
+    private String username;
+    private String password;
+}
